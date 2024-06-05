9f3393faff02cb895a68bc1bbf51d31bac696514
kolen44 <malavov70@gmail.com>
2024-06-05 18:43:05 +0300

    fix: fixed validation for parsint decorator

--- src/decorators/parsint.decorator.ts
+++ src/decorators/parsint.decorator.ts
@@ -4,7 +4,7 @@ import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 export class ParseIntPipe implements PipeTransform<string, number> {
   transform(value: string): number {
     const val = parseInt(value, 10);
-    if (isNaN(val) || value.length !== 8) {
+    if (isNaN(val) || val.toString().length !== 8) {
       throw new BadRequestException(
         'Валидация не пройдена . Проверьте правильность роута. Номер блока должен быть числом и содержать 8 цифр',
       );
